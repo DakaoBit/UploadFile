de72904786eaf3c27cdb021e932538de4325eaf5
oasis.wang <oasis.wang1016@gmail.com>
2022-07-14 21:50:04 +0800

    加入專案檔案。

---
+++ UploadFile/Controllers/FileController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Hosting;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+using UploadFile.Helpers;
+
+namespace UploadFile.Controllers
+{
+    public class FileController : Controller
+    {
+        private IHostingEnvironment _environment;
+
+        public FileController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(List<IFormFile> files)
+        {
+ 
+            //改用 _config 設定圖片路徑
+            //var filePath = Path.Combine(_config["StoredFilesPath"], Path.GetRandomFileName());
+ 
+            var size = files.Sum(f => f.Length);
+
+            foreach (var file in files)
+            {
+                if (file.Length > 0)
+                {
+                    if (!file.IsImage())
+                    {
+                        return Content("Not Image File");
+                    } 
+   
+                    int id = 0;
+                    string wwwRootPath = _environment.WebRootPath;
+                    string extension = Path.GetExtension(file.FileName);
+                    string fileName = $"{id.ToString()}_{DateTime.Now.ToString("yyyyMMddHHmmss")}{extension}";      
+                    string path =  Path.Combine(wwwRootPath + "/img/outertransfer/", fileName);
+
+                    using (var stream = new FileStream(path, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return Ok(new { count = files.Count, size });
+        }
+    }
+}
---
+++ UploadFile/Helpers/IFormFileExtension.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UploadFile.Helpers
+{
+    public static class IFormFileExtension
+    {
+        public const int ImageMinimumBytes = 10000000;// 10 MB
+
+        public static bool IsImage(this IFormFile postedFile)
+        {
+            //-------------------------------------------
+            //  Check the image mime types
+            //-------------------------------------------
+            if (postedFile.ContentType.ToLower() != "image/jpg" &&
+                        postedFile.ContentType.ToLower() != "image/jpeg" &&
+                        postedFile.ContentType.ToLower() != "image/pjpeg" &&
+                        postedFile.ContentType.ToLower() != "image/gif" &&
+                        postedFile.ContentType.ToLower() != "image/x-png" &&
+                        postedFile.ContentType.ToLower() != "image/png")
+            {
+                return false;
+            }
+
+            //-------------------------------------------
+            //  Check the image extension
+            //-------------------------------------------
+            if (Path.GetExtension(postedFile.FileName).ToLower() != ".jpg"
+                && Path.GetExtension(postedFile.FileName).ToLower() != ".png"
+                && Path.GetExtension(postedFile.FileName).ToLower() != ".gif"
+                && Path.GetExtension(postedFile.FileName).ToLower() != ".jpeg")
+            {
+                return false;
+            }
+
+            //-------------------------------------------
+            //  Attempt to read the file and check the first bytes
+            //-------------------------------------------
+            try
+            {
+                //------------------------------------------
+                //check whether the image size exceeding the limit or not
+                //------------------------------------------ 
+                if (postedFile.Length > ImageMinimumBytes)
+                {
+                    return false;
+                }
+
+                byte[] buffer = new byte[ImageMinimumBytes];
+                postedFile.OpenReadStream().Read(buffer, 0, ImageMinimumBytes);
+                string content = System.Text.Encoding.UTF8.GetString(buffer);
+                if (Regex.IsMatch(content, @"<script|<html|<head|<title|<body|<pre|<table|<a\s+href|<img|<plaintext|<cross\-domain\-policy",
+                    RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Multiline))
+                {
+                    return false;
+                }
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
